bf0d0607e6af5a2ad1a78e0e75e3ed2ba10c2dc5
DmitryMurashov <dmtr.murashov@gmail.com>
2025-12-01 17:45:41 +0300

    [tools] launch w/ venv

--- launch.sh
+++ launch.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 mkdir -p ./zephyrproject
-docker run --user nrf52 -it -v $PWD/zephyrproject:/home/nrf52/zephyrproject nrf-build-environment bash -c 'cd ~ ; /bin/bash'
+docker run --user nrf52 -it -v $PWD/zephyrproject:/home/nrf52/zephyrproject nrf-build-environment \
+	bash -c 'export ZEPHYR_BASE=/home/nrf52/zephyrproject/toolchains; \
+	. ~/zephyrproject/venv/bin/activate; cd ~ ; /bin/bash'
